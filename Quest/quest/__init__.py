38e422d98198437af1421664434f9d7d99efb8d1
Kevin-lkw <liankewei2003@gmail.com>
2026-06-20 12:31:47 +0000

    remove quest

--- Quest/quest/__init__.py
+++
@@ -1,7 +0,0 @@
-from quest.models import LlamaForCausalLM
-
-__version__ = "0.0.1"
-
-__all__ = [
-    "LlamaForCausalLM"
-]
--- Quest/quest/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from quest.models.llama import LlamaConfig, LlamaForCausalLM
-
-__all__ = [
-    'LlamaConfig',
-    'LlamaForCausalLM',
-]
